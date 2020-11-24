18d6f1bad159dd4d0a019a4bec2b7a54fb1eed43
Noel Loera <50557498+noelloera@users.noreply.github.com>
2020-11-24 14:34:24 -0700

    Updated binaryTreeClass.js
    Added the Node class as well as the BinarySearchTree Class. In the BST class the fuctionality to add nodes to the binary tree was added through conditionals that check root, left, and right nodes for values. If applicable.

---
+++ binaryTreeClass.js
@@ -0,0 +1,40 @@
+class Node {
+  cosntructor(data, right, left) {
+    this.data = data;
+    this.right = right;
+    this.left = left;
+  }
+}
+class BST {
+  constructor() {
+    this.root = null;
+  }
+  add(data) {
+    const node = this.root;
+    if (node === null) {
+      this.root = new Node(data);
+      return;
+    } else {
+      const searchTree = function (node) {
+        if (data < node.data) {
+          if (node.left === null) {
+            node.left = new Node(data);
+            return;
+          } else if (node.left !== null) {
+            return searchTree(node.left);
+          }
+        } else if (data > node.data) {
+          if (node.right === null) {
+            node.right = new Node(data);
+            return;
+          } else if (node.right !== null) {
+            return searchTree(node.right);
+          }
+        } else {
+          return null;
+        }
+      };
+      return searchTree(node);
+    }
+  }
+}
